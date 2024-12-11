3a9ceede1ac9ad75ccf4a6ac19d88ab408c68c91
Keith Marshall <code@eleusis.uk>
2024-12-11 12:40:09 +0000

    Day 11 complete.

--- src/Day11.kt
+++ src/Day11.kt
@@ -11,16 +11,37 @@ private fun String.splitInHalf(): List<String> =
 
 private fun String.removeLeadingZeroes(): String = BigInteger(this).toString()
 
+private fun blinkOneStone(stone: String): List<String> =
+    when {
+        stone == "0" ->
+            listOf("1")
+        stone.length.isEven() ->
+            stone.splitInHalf().map(String::removeLeadingZeroes)
+        else ->
+            listOf((BigInteger(stone) * 2024).toString())
+    }
+
 private fun blink(stones: List<String>): List<String> {
-    return stones.flatMap { stone ->
-        when {
-            stone == "0" ->
-                listOf("1")
-            stone.length.isEven() ->
-                stone.splitInHalf().map(String::removeLeadingZeroes)
-            else ->
-                listOf((BigInteger(stone) * 2024).toString())
-        }
+    return stones.flatMap(::blinkOneStone)
+}
+
+// key == (stone_value, times), value is the number of stones produced
+private val cache = mutableMapOf<Pair<String, Int>, Long>()
+
+private fun blinkIncrementally(stone: String, times: Int): Long {
+    // We only need to know the number, so can do this piece-wise
+    if (times == 0) {
+        return 1
+    }
+
+    if (cache.contains(Pair(stone, times))) {
+        return cache[Pair(stone, times)]!!
+    }
+
+    return blinkOneStone(stone).sumOf {
+        val result = blinkIncrementally(it, times - 1)
+        cache[Pair(it, times - 1)] = result
+        result
     }
 }
 
@@ -35,8 +56,9 @@ fun main() {
         return stones.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val stones = input[0].split(Regex("\\s+"))
+        return stones.sumOf{ blinkIncrementally(it, 75) }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -48,6 +70,6 @@ fun main() {
     part1(input).println()
 
 
-    check(part2(testInput) == 31)
+//    check(part2(testInput) == 31)
     part2(input).println()
 }
